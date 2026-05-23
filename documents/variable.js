6ab04a2d5bd8431008d0b53a79a672d568e8f807
Nguyễn Đình Long <longnd2711@gmail.com>
2026-05-23 11:43:33 +0700

    +

---
+++ documents/variable.js
@@ -0,0 +1,6 @@
+// Thay đổi các nội dung trong dấu ngoặc kép tùy theo doanh nghiệp hiện tại
+var COMPANY_ABBR = "Tên Viết Tắt Công Ty Khác";
+var DEPARTMENT_NAME = "Phòng IT";
+var DEPARTMENT_SHORT = "Phòng IT";
+var DEPARTMENT_EN = "IT DEPARTMENT"; 
+var APPROVER_TITLE = "Hội Đồng Quản Trị";
